fb08b3539fe3313335f0cf5496af383ad9cbf4ed
Rezky Aulia Pratama <rezkyauliapratama@gmail.com>
2018-12-04 16:54:42 +0800

    Creating custom component

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import ListItem from './src/components/listitem/ListItem';
 
 export default class App extends React.Component {
+  state = {
+    placeName: '',
+    places: []
+  }
+
+  placeNameChangeHandler = textVal => {
+    this.setState({
+      placeName: textVal
+    })
+  }
+
+  placeSubmitHandler = () =>{
+    if (this.state.placeName.trim() === ""){
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      }
+    })
+  }
+
   render() {
-    return (
+    
+    const placesOutput = this.state.places.map((place,index) => (
+      <ListItem key={index} placeName={place}/>
+    ));
+
+    return ( 
       <View style={styles.container}>
-        <Text>Hai my name is rezky</Text>
+        <View style={styles.inputContainer}>
+          <TextInput 
+            style={styles.placeInput}
+            placeholder="An awesome place"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangeHandler}
+            />
+
+          <Button
+            style={styles.placeButton}
+            title="Add"
+            onPress={this.placeSubmitHandler}
+            />
+        </View>
+        
+        {placesOutput}
+        
       </View>
     );
   }
@@ -14,8 +59,25 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 40,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  inputContainer: {
+    // flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    width:"70%",
+    padding: 5,
+    borderWidth: 1,
+    borderColor: "black"
+  },
+  placeButton: {
+    width:"30%",
+  }
 });
